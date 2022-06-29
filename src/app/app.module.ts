39035350d1731bf36420d85d1bce6abaf85843a6
leo-demetrio <leocdemetrio@gmail.com>
2022-06-29 05:30:19 -0300

    Feat: Routes main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { LoginPageComponent } from './pages/account/login-page/login-page.compon
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
-import { ProductsPageComponent } from './store/products-page/products-page.component';
-import { CartPageComponent } from './store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CartPageComponent } from './store/cart-page/cart-page.component';
     SignupPageComponent,
     PetsPageComponent,
     ProductsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
