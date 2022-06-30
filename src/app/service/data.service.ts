1d5257e45fc6205a9f7a15b6d34254050070eb94
leo-demetrio <leocdemetrio@gmail.com>
2022-06-30 18:41:31 -0300

    Feat: Product impots

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,3 +1,4 @@
+import { Product } from './../models/product.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { delay, first, tap } from 'rxjs';
@@ -11,7 +12,7 @@ export class DataService {
     private http: HttpClient
   ) { }
   getProducts() {
-    return this.http.get<any[]>('http://localhost:3000/products')
+    return this.http.get<Product[]>('http://localhost:3000/products')
     .pipe(
       tap(product => console.log(product)))      
   }
